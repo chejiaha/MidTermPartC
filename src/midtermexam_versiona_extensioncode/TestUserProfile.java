23b208085a64cfee8c42e18b10252602112eb0f6
John <chejiaha@sheridancollege.ca>
2019-03-04 13:17:18 -0500

    Formatting fix

--- src/midtermexam_versiona_extensioncode/TestUserProfile.java
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -12,29 +12,30 @@ import java.util.Scanner;
  * @author John
  */
 public class TestUserProfile {
- 
-        public static void main (String[] args){
-            Scanner input = new Scanner(System.in);
-            
-            String name;
-            String genre;
-            String[] genres =  {"Comedy", "Drama", "Action", "Mystery"};
-            
-            System.out.println("enter name");
-            name = input.nextLine();
-            
-            System.out.println("Which of the following genres is your favourite?");
-            for (int i = 0; i < genres.length; i++){
-                System.out.print(genres[i] + " ");
-            }
-            System.out.println();
-            genre = input.nextLine();
-            
-            UserProfile user = new UserProfile(name, genre);
-            user.setUserID(name);
-            user.setGenre(genre);
-            
-            System.out.printf("Account created: Name: %s Genre: %s", 
-                    user.getUserID(), user.getGenre());
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String name;
+        String genre;
+        String[] genres = {"Comedy", "Drama", "Action", "Mystery"};
+
+        System.out.println("enter name");
+        name = input.nextLine();
+
+        System.out.println("Which of the following genres is your favourite?");
+        for (int i = 0; i < genres.length; i++) {
+            System.out.print(genres[i] + " ");
         }
+        System.out.println();
+        genre = input.nextLine();
+
+        UserProfile user = new UserProfile(name, genre);
+        user.setUserID(name);
+        user.setGenre(genre);
+
+        System.out.printf("Account created: Name: %s Genre: %s",
+                user.getUserID(), user.getGenre());
+
+    }
 }
